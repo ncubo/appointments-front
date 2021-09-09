d68879036717b44e1c7d3d769a131bb6b88cfacb
Miguel Mayorga <mmayorga@gmail.com>
2021-09-09 10:53:14 -0300

    before create branch lint

--- src/app/services/professionals.service.ts
+++ src/app/services/professionals.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.dev';
 
 const base_url = environment.base_url;
 
