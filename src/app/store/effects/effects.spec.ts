2bcf0c90fdea9828d1ec777a9b449479858b3c09
Miguel Mayorga <mmayorga@gmail.com>
2021-10-04 18:09:39 -0300

    adding testing with effects

--- src/app/store/effects/effects.spec.ts
+++ src/app/store/effects/effects.spec.ts
@@ -1,38 +1,83 @@
-import * as fromEffects from './index';
-import * as actions from '../actions';
-// import { ProfessionalState } from './professional.reducer';
+import { TestBed } from '@angular/core/testing';
+import { ProfessionalEffects } from './professional.effects';
+
+import * as actionsIndex from '../actions';
+import { Observable } from 'rxjs';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { ProfessionalsService } from 'src/app/services/professionals.service';
+import { TestScheduler } from 'rxjs/testing';
+
 
 describe('Professional Effects', () => {
 
-//   describe('unknown action', () => {
-//     it('should return the default state for professional reducer', () => {
-//       const { professionalInitialState } = fromReducer;
-//       const action = {
-//         type: 'Unknown'
-//       };
-//       const state = fromReducer.professionalReducer(professionalInitialState, action);
+    const initialState = { 
+        professional: { avatar:'', first_name:'', city:'', last_name:'', services: [] },
+        loaded: false,
+        loading: false,
+        state: 'waiting',
+        error: { url: '', name: '', message: '' }
+    };
 
-//       expect(state).toBe(professionalInitialState);
-//     });
-//   });
+    const professionalService = jasmine.createSpyObj('ProfessionalsService', [
+        'insertNewProfessional',
+        'getProfessionalsById'
+    ]);
 
-//   describe('newProfessional action', () => {
-//     it('should update the state in an immutable way', () => {
-//       const { professionalInitialState } = fromReducer;
-//       const newState: ProfessionalState = {
-//           professional: { id: 1, avatar:'', first_name:'Test', city:'Test', last_name:'Test', services: [] },
-//           loaded: false,
-//           loading: true,
-//           state: 'waiting',
-//           error: { url: '', name: '', message: '' }
-//         };
+    let effects: ProfessionalEffects;
+    let actions: Observable<any>;
+    let store: MockStore<any>;
+    let testScheduler: any;
 
-//       const action = actions.newProfessional( { professional: newState.professional });
-//       const state = fromReducer.professionalReducer(professionalInitialState, action);
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                ProfessionalEffects,
+                provideMockStore({ initialState }),
+                provideMockActions(() => actions),
+                { provide: ProfessionalsService, useValue: professionalService }
+            ]
+        });
 
-//       expect(state).toEqual(newState);
-//       expect(state).not.toBe(newState);
-//     });
-//   });
+        effects = TestBed.inject(ProfessionalEffects);
+        store = TestBed.inject(MockStore);
+        store.setState(initialState);
+
+        testScheduler = new TestScheduler((actual, expected) => {
+            expect(actual).toEqual(expected);
+        });
+    });
+
+    it('should handle newProfessional and after get newProfessionalSuccess', () => {
+        const newProf = { avatar:'avatar-here', city:'World', first_name: 'Peter', last_name:'Peterson', services:[{ aboutme:'Hi there!', service: 'Professor', timetable:'9-10' }] };
+        const action = actionsIndex.newProfessional({ professional: newProf });
+        const outcome = actionsIndex.newProfessionalSuccess();
+
+        testScheduler.run(({ hot, cold, expectObservable } : { hot: Function, cold: Function, expectObservable: Function }) => {
+            // -a-b----- (every dash for example could be 10 miliseconds)
+            // -a
+            //  -b
+            // --b
+            actions = hot('-a', { a: action });
+            const response = cold('-b|', { b: newProf });
+            professionalService.insertNewProfessional.and.returnValue(response);
+
+            expectObservable(effects.newProfessionals$).toBe('--b', { b: outcome });
+        });
+    });
+
+
+
+});
+
+
+describe('Professionals Effects', () => {
+
+
+});
+
+
+describe('searchProfessionals Effects', () => {
+
 
 });
--- src/app/store/reducers/reducers.spec.ts
+++ src/app/store/reducers/reducers.spec.ts
@@ -1,6 +1,4 @@
 import * as fromReducer from './index';
-import * as actions from '../actions';
-import { ProfessionalState } from './professional.reducer';
 
 describe('Professional Reducer', () => {
 
@@ -13,25 +11,6 @@ describe('Professional Reducer', () => {
       const state = fromReducer.professionalReducer(professionalInitialState, action);
 
       expect(state).toBe(professionalInitialState);
-    });
-  });
-
-  describe('newProfessional action', () => {
-    it('should update the state in an immutable way', () => {
-      const { professionalInitialState } = fromReducer;
-      const newState: ProfessionalState = {
-          professional: { id: 1, avatar:'', first_name:'Test', city:'Test', last_name:'Test', services: [] },
-          loaded: false,
-          loading: true,
-          state: 'waiting',
-          error: { url: '', name: '', message: '' }
-        };
-
-      const action = actions.newProfessional( { professional: newState.professional });
-      const state = fromReducer.professionalReducer(professionalInitialState, action);
-
-      expect(state).toEqual(newState);
-      expect(state).not.toBe(newState);
     });
   });
 
